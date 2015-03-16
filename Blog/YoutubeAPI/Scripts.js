c74f22dc0c9f794eecf5fd883295a312161d3800
SandraMuller <mursan03@gmail.com>
2015-03-17 11:21:50 +1300

    test player

--- Blog/YoutubeAPI/Scripts.js
+++ Blog/YoutubeAPI/Scripts.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
     function LoadPlayer() {
       $('.player').children().remove();
       $('.player').append('<p><iframe id="ytplayer" type="text/html" width="640" height="390" ' +
-      ' src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"/></p>');
+      ' src="http://www.youtube.com/embed/0pajUiPYjrI" frameborder="0"/></p>');
+      //http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com
     }; 
 
     function search() {
